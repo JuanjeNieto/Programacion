7694f600a6fbabb90fb8f8f2b8d46e72027ce190
Juanje <102350305+JuanjeNieto@users.noreply.github.com>
2022-09-22 11:41:18 +0200

    Add files via upload

--- Programación Estructurada/Tema 3 - lectura de datos/Ejercicio10y11/src/Main.java
+++ Programación Estructurada/Tema 3 - lectura de datos/Ejercicio10y11/src/Main.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-      Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner (System.in);
 
         System.out.printf("Dame la cantidad en Mb ");
         double Mb = Double.parseDouble(s.next());
 
-        System.out.println("La cantidad en Kb es " + (Mb*1000));
+        System.out.println("La cantidad en Kb es " + (Mb/1000));
 
         System.out.printf("Dame la cantidad en Kb ");
         double Kb = Double.parseDouble(s.next());
 
-        System.out.println("La cantidad en Mb es " + (Kb/1000));
+        System.out.println("La cantidad en Mb es " + (Kb*1000));
     }
 }

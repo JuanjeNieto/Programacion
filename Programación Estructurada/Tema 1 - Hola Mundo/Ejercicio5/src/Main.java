784d0a09ce2b8877e4c9d83f8fd9674c6cbb70ed
Juanje <102350305+JuanjeNieto@users.noreply.github.com>
2022-09-22 10:13:36 +0200

    Add files via upload

--- Programación Estructurada/Tema 1 - Hola Mundo/Ejercicio5/src/Main.java
+++ Programación Estructurada/Tema 1 - Hola Mundo/Ejercicio5/src/Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Database\tEntornos\tProg.\tEntornos\tDatabase");
-        System.out.println("Database\tEntornos\tProg.\tProg.\t\tDatabase");
-        System.out.println("Inglés \t\t Prog.\t\tProg.\tProg.\t\tSistemas");
+        String rojo = "\033[31m";
+        String verde = "\033[32m";
+        String naranja = "\033[33m";
+        String azul = "\033[34m";
+        String morado = "\033[35m";
+        String gris = "\033[37m";
+        String blanco = "\033[38m";
+
+
+
+        System.out.println(rojo + "Database" + verde + "\tEntornos" + azul +"\tProg." + verde + "\tEntornos" + rojo + "\tDatabase");
+        System.out.println(rojo +"Database"+ verde +"\tEntornos"+azul +"\tProg."+azul +"\tProg."+rojo+ "\t\tDatabase");
+        System.out.println(morado + "Inglés"+azul +" \t\t Prog."+azul +"\t\tProg."+ azul +"\tProg."+naranja+"\t\tSistemas");
         System.out.println("--------------------------------------------------------");
-        System.out.println(" FOL\t\t Prog.\t\tInglés\t\tProg.\t\tSistemas");
-        System.out.println(" FOL\t\t LMSGI.\t\tSistemas\tSistemas\tLMSGI");
-        System.out.println(" FOL\t\t LMSGI.\t\tSistemas\tSistemas\tLMSGI");
+        System.out.println( blanco + " FOL "+azul+"\t\t Prog."+morado+"\t\tInglés"+azul+"\t\tProg."+naranja+"\t\tSistemas");
+        System.out.println(blanco + " FOL"+ gris +"\t\t LMSGI."+naranja +"\t\tSistemas"+naranja+"\tSistemas"+gris+"\tLMSGI");
+        System.out.println(blanco + " FOL"+ gris+"\t\t LMSGI."+naranja +"\t\tSistemas"+naranja+"\tSistemas"+gris+"\tLMSGI");
+
     }
 }
---
+++ Programación Estructurada/Tema 1 - Hola Mundo/Ejercicio9/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        String naranja = "\033[33m";
+        String rojo = "\033[31m";
+
+
+        System.out.println(naranja +   "                  -----------");
+        System.out.println(naranja +   "              -------------------  ");
+        System.out.println(naranja +   "           -------------------------");
+        System.out.println(naranja +   "         -----------------------------" );
+        System.out.println( rojo    +  "        ===============================" );
+        System.out.println( rojo    +  "        ===============================" );
+        System.out.println(naranja + "----------------------------------------------------");
+
+        System.out.println("╔═╦╦╦═╦═╗╔═╗╔═╦═╦═╦═╦═╗");
+        System.out.println("║║║║║║║╚╣║║║║═║║║╚╣═╣╚╣");
+        System.out.println("╠╗║║║║╠╗║║║║║╚╣║╠╗║═╬╗║");
+        System.out.println("╚═╩═╩╩╩═╝╚╩╝╚╩╩═╩═╩═╩═╝");
+
+       
+
+
+
+
+    }
+}

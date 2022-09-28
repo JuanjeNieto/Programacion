82ae2800672e2a6199b7243f54cd09d7fb7b0620
Juanje <102350305+JuanjeNieto@users.noreply.github.com>
2022-09-28 09:37:11 +0200

    Add files via upload

--- Programación Estructurada/Tema 4 _ Condicionales/Ejercicio10/src/Main.java
+++ Programación Estructurada/Tema 4 _ Condicionales/Ejercicio10/src/Main.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-       Scanner s = new Scanner(System.in);
-        String horos;
+        Scanner s = new Scanner(System.in);
+        String horos = "Horoscopo";
         System.out.println("Dime el mes en que naciste(1-12): ");
         int mes = Integer.parseInt(s.next());
 
@@ -25,18 +25,18 @@ public class Main {
                 break;
             case 3:
                 if(dia > 20 ){
-                horos = "Aries";
+                    horos = "Aries";
                 }
                 else{
                     horos = "Piscis";
-                    }break;
+                }break;
             case 4:
                 if (dia > 19) {
                     horos = "Tauro";
                 }
                 else {
                     horos = "Aries";
-            } break;
+                } break;
             case 5:
                 if (dia > 20) {
                     horos = "Géminis";
@@ -58,8 +58,46 @@ public class Main {
                 else{
                     horos = "Cancer";
                 }
+            break;
+            case 8:
+                if (dia > 22) {
+                    horos = "Virgo";
+                }
+               else{
+                    horos = "Leo";
+                } break;
+            case 9:
+                if (dia > 22) {
+                    horos = "Libra";
 
-                // Continuar mañana
+                }else{
+                    horos = "Virgo";
+                } break;
+            case 10:
+                if (dia > 22) {
+                    horos = "Escorpio";
+
+                }else{
+                    horos = "Libra";
+                } break;
+            case 11:
+                if (dia > 21) {
+                    horos = "Sagitario";
+
+                }else{
+                    horos = "Escorpio";
+                } break;
+            case 12:
+                if (dia > 21) {
+                    horos = "Cpricornio";
+                }
+                else{
+                    horos = "Sagitario";
+                }break;
+            default:
+                System.out.println("Este mes no existe."); break;
         }
+        System.out.println("Tu horoscopo es: " + horos);
+
     }
 }

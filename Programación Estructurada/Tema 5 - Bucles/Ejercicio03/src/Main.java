3bf88f448463fee3d11af704afc08bf581b78817
Juanje <102350305+JuanjeNieto@users.noreply.github.com>
2022-09-29 13:11:04 +0200

    Add files via upload

--- Programación Estructurada/Tema 5 - Bucles/Ejercicio03/src/Main.java
+++ Programación Estructurada/Tema 5 - Bucles/Ejercicio03/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
+        int i = -5;
         do {
             System.out.println(i+=5);
         }while(i < 100);
---
+++ Programación Estructurada/Tema 5 - Bucles/Ejercicio05/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 320;
+        while (i > 160){
+            i-=20;
+            System.out.println(i);
+        }
+    }
+}

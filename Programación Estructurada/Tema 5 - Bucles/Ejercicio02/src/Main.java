481f0f00b7fdc1343d69b2d89d58e091b3b01d5d
Juanje <102350305+JuanjeNieto@users.noreply.github.com>
2022-09-29 12:58:15 +0200

    Add files via upload

--- Programación Estructurada/Tema 5 - Bucles/Ejercicio02/src/Main.java
+++ Programación Estructurada/Tema 5 - Bucles/Ejercicio02/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-       while ()
+        int i = 0;
+       while (i <100){
+           System.out.println(i+=5);
+        }
     }
 }

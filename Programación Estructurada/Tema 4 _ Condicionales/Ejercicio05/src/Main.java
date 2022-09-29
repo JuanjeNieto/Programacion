ee8c763d512403b9ffb6f3108b8e432f22623f3d
Juanje <102350305+JuanjeNieto@users.noreply.github.com>
2022-09-29 17:24:40 +0200

    Add files via upload

--- Programación Estructurada/Tema 4 _ Condicionales/Ejercicio05/src/Main.java
+++ Programación Estructurada/Tema 4 _ Condicionales/Ejercicio05/src/Main.java
@@ -4,6 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        
+        System.out.printf("Indica el valor de a: ");
+        double a = Double.parseDouble(s.next());
+
+        System.out.printf("Indica el valor de b: ");
+        double b = Double.parseDouble(s.next());
+
+        if (a == 0){
+            System.out.println("La ecuación no puede tener solución.");
+        }else {
+            System.out.println("El valor de x = " + (-b/a));
+        }
     }
 }

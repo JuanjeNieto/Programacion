16ad5bb318dc634ea3ce699980a5d734e8c0f915
Juanje <102350305+JuanjeNieto@users.noreply.github.com>
2022-10-04 11:49:58 +0200

    Add files via upload

--- Programación Estructurada/Tema 5 - Bucles/Ejercicio13/src/Main.java
+++ Programación Estructurada/Tema 5 - Bucles/Ejercicio13/src/Main.java
@@ -3,6 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        
+
+        int contNegativo = 0;
+        int contPositivo = 0;
+        int num = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Dime un número: ");
+            num = Integer.parseInt(s.next());
+
+            if (num > 0) {
+                contPositivo++;
+            }
+            else {
+                contNegativo++;
+            }
+        }
+        System.out.println("Tienes "+contPositivo+" números positivos");
+        System.out.println("Tienes "+contNegativo+" números negativos");
     }
-}
+
+    }

5831f1819f6aeb53a28916a3b96c73886fc04a06
Juanje <102350305+JuanjeNieto@users.noreply.github.com>
2022-10-06 09:44:42 +0200

    Update Main.java

--- Programación Estructurada/Tema 5 - Bucles/Ejercicio19/src/Main.java
+++ Programación Estructurada/Tema 5 - Bucles/Ejercicio19/src/Main.java
@@ -10,11 +10,13 @@ public class Main {
 
         System.out.println("Introduce la altura de la pirámide: ");
         int altura = Integer.parseInt(s.next());
-
+// Hacemos la piramide con bucles
         for (int i = 0; i < altura; i++) {
+        // El bucle para los espacios
             for (int j = 0; j < altura-i-1; j++) {
                 System.out.print(" ");
             }
+        // Y el bucle para ir sumando los caracteres    
             for (int j = 0; j < 2*i+1; j++) {
                 System.out.print(caracter);
             }

8c0e2b2666ed11f2390e61037acce901760fc7c2
Juanje <102350305+JuanjeNieto@users.noreply.github.com>
2022-10-19 08:54:54 +0200

    Update Main.java

--- Programación Estructurada/Tema 5 - Bucles/Ejercicio11/src/Main.java
+++ Programación Estructurada/Tema 5 - Bucles/Ejercicio11/src/Main.java
@@ -4,17 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        int num = 0;
-
         int cuadrado = 0;
 
         int cubo = 0;
 
-
-
-        for (int i = 0; i < 5; i++) {
             System.out.print("Dime un número: ");
-            num = Integer.parseInt(s.next());
+            int num = Integer.parseInt(s.next());
+
+        for (int i = num; i <= 5; i++) {
+            
 
             cuadrado = num * num;
             cubo = num * num * num;

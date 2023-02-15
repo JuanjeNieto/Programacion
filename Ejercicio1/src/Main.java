2583f3374aad4046adf90a9d2f00f0ff38d43ff6
Juanje <102350305+JuanjeNieto@users.noreply.github.com>
2023-02-15 10:35:28 +0100

    Delete Ejercicio1 directory

--- Ejercicio1/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Caballo rocinante = new Caballo("Mustang", "Negro", "1234", 23);
-
-        System.out.println(rocinante);
-        rocinante.salta();
-    }
-}
